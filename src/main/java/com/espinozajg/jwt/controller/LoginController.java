2ab2376b42412a101774344d0df5e9cbc13ff354
Jose Espinoza <espinozajgx@gmail.com>
2023-06-04 17:55:19 +0200

    fix(jwt) clean code

---
+++ src/main/java/com/espinozajg/jwt/controller/LoginController.java
@@ -0,0 +1,31 @@
+package com.espinozajg.jwt.controller;
+
+import com.espinozajg.jwt.dto.User;
+import com.espinozajg.jwt.security.JWTAuthtenticationConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+	@Autowired
+	JWTAuthtenticationConfig jwtAuthtenticationConfig;
+
+	@PostMapping("login")
+	public User login(
+			@RequestParam("user") String username,
+			@RequestParam("password") String pass) {
+		
+		String token = jwtAuthtenticationConfig.getJWTToken(username);
+		User user = new User();
+		user.setUser(username);
+		user.setPass(pass);
+		user.setToken(token);		
+		return user;
+		
+	}
+
+
+}
---
+++ src/main/java/com/espinozajg/jwt/dto/User.java
@@ -0,0 +1,39 @@
+package com.espinozajg.jwt.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class User {
+
+	@JsonIgnore
+	private String user;
+
+	@JsonIgnore
+	private String pass;
+
+	private String token;
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+}
